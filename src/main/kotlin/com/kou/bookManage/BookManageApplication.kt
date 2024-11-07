345708c85d5caf4b5a80216bf502495e4f934cee
rivel-007 <kamls27849@gmail.com>
2024-11-07 20:07:42 +0900

    初期設定

---
+++ src/main/kotlin/com/kou/bookManage/BookManageApplication.kt
@@ -0,0 +1,11 @@
+package com.kou.bookManage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookManageApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookManageApplication>(*args)
+}
---
+++ src/test/kotlin/com/kou/bookManage/BookManageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kou.bookManage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookManageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
